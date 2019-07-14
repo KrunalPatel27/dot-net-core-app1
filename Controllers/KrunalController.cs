4b144879fc4184762921a0300137da1b91f1e63a
KrunalPatel27 <kpate236@gmail.com>
2019-07-13 20:14:47 -0500

    Add project files.

---
+++ Controllers/KrunalController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KrunalController : ControllerBase
+    {
+
+
+        // GET api/values
+        [EnableCors("AnotherPolicy")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Krunal1", "Krunal27" };
+        }
+
+        // GET api/values/5
+        [EnableCors("AnotherPolicy")]
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "Krunal7";
+        }
+    }
+}
